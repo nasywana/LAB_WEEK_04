0905a806ace21450867c2f3aa6abb201f532a1db
nasywana <131771525+nasywana@users.noreply.github.com>
2024-09-17 18:50:42 +0700

    Update CafeAdapter.kt

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -17,3 +17,14 @@ val TABS_CONTENT_FIXED = listOf(
     R.string.janjijiwa_desc,
     R.string.kopikenangan_desc,
 )
+
+class CafeAdapter(private val context: Context, fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return TABS_FIXED.size
+    }
+    override fun createFragment(position: Int): Fragment
+    {
+        return CafeDetailFragment.newInstance(context.getString(TABS_CONTENT_FIXED[position]))
+    }
+}
