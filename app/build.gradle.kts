a6f3ed716c9d212c375a497f9d43b414a33f3601
lindasun31 <129498464+lindasun31@users.noreply.github.com>
2024-09-17 17:05:52 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 
 }
